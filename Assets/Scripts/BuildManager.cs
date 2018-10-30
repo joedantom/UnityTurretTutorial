b4d81fd42ebb6f385e4bb095eb3291b2f517b340
jtapollo <34532666+jtapollo@users.noreply.github.com>
2018-10-30 16:53:04 -0700

    Main Upload
    Turret Tutorial Though EP12

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if (instance !=null)
+        {
+            Debug.LogError("More than one Build manager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject gunTurretPrefab;
+    public GameObject missileTurretPrefab;
+
+
+    private TurretBlueprint turretToBuild;
+
+    public bool CanBuild { get { return turretToBuild != null; } }
+    public bool HasMoney { get { return PlayerStats.Money >= turretToBuild.cost; } }    
+
+    public void BuildTurretOn (Node node)
+    {
+        if (PlayerStats.Money < turretToBuild.cost)
+        {
+            Debug.Log ("Not Enough Money");
+            return;
+        }
+
+        PlayerStats.Money -= turretToBuild.cost;
+
+        GameObject turret = (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPostion(), Quaternion.identity);
+        node.turret = turret;
+
+        Debug.Log("Turret built. Money Left" + PlayerStats.Money);
+    }
+
+    public void SelectTurretToBuild (TurretBlueprint turret)
+    {
+        turretToBuild = turret;
+    }
+
+}
---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+    public TurretBlueprint gunTurret;
+    public TurretBlueprint missileTurret;
+
+    BuildManager buildManager;
+
+    private void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+    public void SelectGunTurret ()
+    {
+        Debug.Log("Gun Turret Purchased");
+        buildManager.SelectTurretToBuild(gunTurret);
+    }
+
+    public void SelectMissleTurret()
+    {
+        Debug.Log("Missle Turret Purchased");
+        buildManager.SelectTurretToBuild(missileTurret);
+    }
+
+}
